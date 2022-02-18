35c9834efdaa8e5f1523adac194e70ed80a87f3e
juanp-ctrl <juanpa11121@hotmail.com>
2022-02-18 10:12:09 -0500

    Vista de login agregada

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let ele2, ele, menu;
+let ele2, ele, menu, v_login, v_home, entrar, vistas = [];
 
 //Iniciar la ejecucion de la funcion cuando la pagina cargue por completo
 document.addEventListener('readystatechange', function() {
@@ -6,9 +6,29 @@ document.addEventListener('readystatechange', function() {
         ele2 = document.getElementsByTagName("div")[0];
         ele = ele2.getElementsByTagName("div")[0];
         menu = document.getElementsByTagName("nav")[0];
+        v_login = document.getElementById("vista_login");
+        v_home = document.getElementById("vista_home");
+        vistas.push(v_login);
+        vistas.push(v_home);
         init();
+        cargarVistas();
     }
 });
+
+function cargarVistas(){
+    for(let i = 0; i<vistas.length; i++){
+        vistas[i].classList.add("hide");
+    }
+    vistas[0].classList.remove("hide");
+    entrar = document.getElementById("Entrar").addEventListener("click", vistaHome);
+}
+
+function vistaHome(){
+    for(let i = 0; i<vistas.length; i++){
+        vistas[i].classList.add("hide");
+    }
+    vistas[1].classList.remove("hide");
+}
 
 //Funcion para cambiar el estado del menu
 function toggleMenu() {
